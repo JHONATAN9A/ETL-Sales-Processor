9030069e76cd695bb65de00276f09c8eb3746115
JHONATAN9A <calderonjonatan99@gmail.com>
2026-03-16 00:59:40 -0500

    ADD file csv 1000 registros

--- files/generate_csv.py
+++ files/generate_csv.py
@@ -2,8 +2,8 @@ import csv
 import random
 from datetime import date, timedelta
 
-ROWS = 1_000_000
-OUTPUT = "test_large.csv"
+ROWS = 1_000
+OUTPUT = "test_large_2.csv"
 
 start_date = date(2026, 1, 1)
 product_ids = list(range(1001, 1101))  # 100 productos
